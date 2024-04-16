4d00565d7358fff3d6f8edddcb8b040c908adf9d
rileysurratt <rileysurratt13@gmail.com>
2024-04-16 17:56:43 -0400

    inital commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// TODO: freelancer array -- name, occupation, starting price
+// TODO: message displays average starting price of all freelancers
+// TODO: set in
+// TODO: new listing appears every few seconds & average price is updated
+
