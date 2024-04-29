aeb4ee3414c8cec1c03302ceafe8cbc357cfddfb
rileysurratt <rileysurratt13@gmail.com>
2024-04-29 18:45:31 -0400

    updated changes

--- index.js
+++ index.js
@@ -18,18 +18,18 @@ function addFreelancer(freelancers) {
         // selects the element we want to target to create new row with DOM
         const table = document.querySelector("table")
         // removes the first element in freelancers array and then returns the removed element
-        const currentFreelancers = freelancers.pop();
+        const newFreelancers = freelancers.slice();
+        const currentFreelancers = newFreelancers.pop();
         // creates a new listing in the table 
         const newListing = document.createElement("tr")
         newListing.innerHTML = `<td>${currentFreelancers.name}</td> <td>${currentFreelancers.occupation}</td> <td>${currentFreelancers.price}</td>`
         table.append(newListing);
         // Updates the average starting price when new listing is created
-        // FIX ME: figure out why listings dont display
-        
+        totalAveragePrice();
 
 }
 // New listing appears every 3 seconds
-const freelancerInterval = setInterval(() => addFreelancer(freelancers), 3000)
+const freelancerInterval = setInterval(() => addFreelancer(freelancers), 1000)
 
 // TODO: message displays average starting price of all freelancers
 function totalAveragePrice() {
@@ -39,8 +39,11 @@ function totalAveragePrice() {
         const totalPrice = parseInt(currentFreelancer.price.substring(2));
         total += totalPrice;
     }
-    const average = total / freelancers.length
+    const average = total / freelancers.length;
+    console.log("Average Price:", average)
 }
+
+
 
 
 
